77a2bc8911191581b9f4e642e4ea170bee21cfc1
Jean-François Hren <jfhren@gmail.com>
2013-11-22 01:06:30 +0100

    The previous commit would have been better with a '-a'

--- src/display.h
+++ src/display.h
@@ -1,3 +1,10 @@
+/**
+ * Copyright © 2013 Jean-François Hren <jfhren@gmail.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 /* Initialize ncurses
  * code       : the code to display
  * codeSize   : the size of aforementioned code
--- src/get_file_content.h
+++ src/get_file_content.h
@@ -1,1 +1,8 @@
+/**
+ * Copyright © 2013 Jean-François Hren <jfhren@gmail.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 char* getFileContent(const char* filename);
